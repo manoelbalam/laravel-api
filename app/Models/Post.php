1a6e7d412b649250fe8c3a4cc4959ab0d042a76a
isaac balam <manoel.balam@gmail.com>
2025-08-02 23:13:28 -0500

    add relations and post controller logic

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -1,10 +1,16 @@
 <?php
 
 namespace App\Models;
+use App\Models\User;
 
+// Allow mass assignment for these fields
+protected $fillable = ['user_id', 'title', 'body'];
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
